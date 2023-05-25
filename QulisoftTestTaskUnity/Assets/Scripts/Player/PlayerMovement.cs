d06bf3f2479cf925c3ced18a8394e02af5354fad
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-05-25 17:35:01 +0300

    added player rotation

--- QulisoftTestTaskUnity/Assets/Scripts/Player/PlayerMovement.cs
+++ QulisoftTestTaskUnity/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,17 +4,21 @@ namespace QulisoftTestTask.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
+        [SerializeField] private Rigidbody2D _wholeCar;
+        
         [SerializeField] private Rigidbody2D _frontWheel;
         [SerializeField] private Rigidbody2D _backWheel;
 
         [SerializeField] private float _speed;
+        [SerializeField] private float _rotationSpeed;
         
         public void Move(bool isMovingForward)
         {
             int direction = isMovingForward ? 1 : -1;
 
-            _frontWheel.AddTorque(-direction * _speed * Time.deltaTime);
-            _backWheel.AddTorque(-direction * _speed * Time.deltaTime);
+            _frontWheel.AddTorque(-direction * _speed * Time.fixedDeltaTime);
+            _backWheel.AddTorque(-direction * _speed * Time.fixedDeltaTime);
+            _wholeCar.AddTorque(-direction * _rotationSpeed * Time.fixedDeltaTime);
         }
     }
 }
