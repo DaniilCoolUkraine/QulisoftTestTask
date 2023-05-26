a955bc40d381eba69704f140933f7787c40b6edb
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-05-26 12:30:20 +0300

    added restart button controller

---
+++ QulisoftTestTaskUnity/Assets/Scripts/Ui/ButtonControllers/ButtonController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace QulisoftTestTask.Ui.ButtonControllers
+{
+    public abstract class ButtonController : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(DoAction);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(DoAction);
+        }
+
+        protected abstract void DoAction();
+    }
+}
