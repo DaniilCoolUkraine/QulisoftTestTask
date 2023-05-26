0f1014b39793d29aaed9989670d589e5b501b843
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-05-26 13:29:07 +0300

    rewrited ui player input

--- QulisoftTestTaskUnity/Assets/Scripts/Player/PlayerController.cs
+++ QulisoftTestTaskUnity/Assets/Scripts/Player/PlayerController.cs
@@ -1,21 +1,37 @@
 ﻿using QulisoftTestTask.Player.Input;
+using QulisoftTestTask.ScriptableObjects.Events;
 using UnityEngine;
 
 namespace QulisoftTestTask.Player
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private PlayerInput _playerInput;
+        [SerializeField] private BoolEventSO _onAccelerateButtonIsHolding;
+        [SerializeField] private BoolEventSO _onBrakeButtonIsHolding;
+        
+        [SerializeField] private PlayerInput[] _playerInputs;
         [SerializeField] private PlayerMovement _playerMovement;
 
         private void OnEnable()
         {
-            _playerInput.IsMovingForward += _playerMovement.Move;
+            _onAccelerateButtonIsHolding.OnValueChanged += _playerMovement.Move;
+            _onBrakeButtonIsHolding.OnValueChanged += _playerMovement.Move;
+            
+            foreach (PlayerInput playerInput in _playerInputs)
+            {
+                playerInput.IsMovingForward += _playerMovement.Move;
+            }
         }
         
         private void OnDisable()
         {
-            _playerInput.IsMovingForward -= _playerMovement.Move;
+            _onAccelerateButtonIsHolding.OnValueChanged -= _playerMovement.Move;
+            _onBrakeButtonIsHolding.OnValueChanged -= _playerMovement.Move;
+            
+            foreach (PlayerInput playerInput in _playerInputs)
+            {
+                playerInput.IsMovingForward -= _playerMovement.Move;
+            }
         }
     }
 }
--- QulisoftTestTaskUnity/Assets/Scripts/Ui/UiManager.cs
+++ QulisoftTestTaskUnity/Assets/Scripts/Ui/UiManager.cs
@@ -1,4 +1,5 @@
 ﻿using QulisoftTestTask.ScriptableObjects.Events;
+using QulisoftTestTask.Ui.ButtonControllers;
 using UnityEngine;
 
 namespace QulisoftTestTask.Ui
@@ -8,21 +9,33 @@ namespace QulisoftTestTask.Ui
         [Header("Events")]
         [SerializeField] private FloatEventSO _fuelAmountChanged;
         [SerializeField] private EventSO _onPlayerLoose;
-
+        
+        [SerializeField] private BoolEventSO _onAccelerateButtonIsHolding;
+        [SerializeField] private BoolEventSO _onBrakeButtonIsHolding;
+        
         [Header("Components")] 
         [SerializeField] private UiFuelShower _uiFuelShower;
         [SerializeField] private ScreenController _looseScreenController;
+
+        [SerializeField] private ButtonHoldingDetection _accelerateButtonIsHolding;
+        [SerializeField] private ButtonHoldingDetection _brakeButtonIsHolding;
 
         private void OnEnable()
         {
             _fuelAmountChanged.OnValueChanged += _uiFuelShower.UpdateFuelFill;
             _onPlayerLoose.OnInvoked += _looseScreenController.SwitchScreen;
+
+            _accelerateButtonIsHolding.OnButtonIsHolding += _onAccelerateButtonIsHolding.ChangeValue;
+            _brakeButtonIsHolding.OnButtonIsHolding += _onBrakeButtonIsHolding.ChangeValue;
         }
         
         private void OnDisable()
         {
             _fuelAmountChanged.OnValueChanged -= _uiFuelShower.UpdateFuelFill;
             _onPlayerLoose.OnInvoked -= _looseScreenController.SwitchScreen;
+            
+            _accelerateButtonIsHolding.OnButtonIsHolding -= _onAccelerateButtonIsHolding.ChangeValue;
+            _brakeButtonIsHolding.OnButtonIsHolding -= _onBrakeButtonIsHolding.ChangeValue;
         }
     }
 }
